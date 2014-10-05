4b582a15581dee7039f7743c5111d9305131362c
Seth Musulin <sethmusulin@gmail.com>
2014-10-05 12:30:48 -0600

    Trying different styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  resources :playlists do
-    resources :songs
-  end
+  resources :songs, only: [:new, :create]
+  resources :playlists 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
