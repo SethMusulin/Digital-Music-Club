0b2f020e6994a7d20be38f6fa11fb2abaf554f46
Seth Musulin <sethmusulin@gmail.com>
2014-10-13 01:28:48 -0600

    combining tests

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 feature "User sign up" do
-  scenario "user can add him or herself to the club" do
+  scenario "user can add him or herself to the club with e-mail address" do
     visit '/'
+
+    fill_in "user[email]", with: ""
+
+    click_button ""
+
+    expect(User.all.length).to eq(0)
 
     fill_in "user[email]", with: "joe@example.com"
 
@@ -11,13 +17,5 @@ feature "User sign up" do
     expect(User.first.email).to eq("joe@example.com")
     expect(User.all.length).to eq(1)
   end
-  scenario "user cannot be created with blank email" do
-    visit '/'
 
-    fill_in "user[email]", with: ""
-
-    click_button ""
-
-    expect(User.all.length).to eq(0)
-  end
 end
