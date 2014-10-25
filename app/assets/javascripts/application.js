7b717d18064c0d647f5f03967ee8ff9e77349f73
Seth Musulin <sethmusulin@gmail.com>
2014-10-25 16:51:10 -0600

    adding animation for tracks toggle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,7 @@ $(document).ready(function () {
 
   $('h1').animate({right: 210}, 1300);
   $('.vert-line').animate({left: 293}, 1000);
+  $('.side-toggle').animate({bottom: 400}, 1000);
 
 
 //player toggle
