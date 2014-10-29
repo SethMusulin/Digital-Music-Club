cda1d57fdf553d058d56a53ed70a10d1d3829bba
Seth Musulin <sethmusulin@gmail.com>
2014-10-29 13:47:11 -0600

    Adding flash message upon adding user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new
     @user.email = params[:user][:email]
-    @user.save
-    redirect_to root_path
+    if @user.save
+      flash[:notice] = "Thanks for joining the DMC, you have been added to the list"
+      redirect_to root_path
+    end
+
 
   end
 end
