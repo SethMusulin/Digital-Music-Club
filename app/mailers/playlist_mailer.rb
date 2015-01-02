0e858402b5331725b3eb0f7d128776e589671ad9
Seth Musulin <sethmusulin@gmail.com>
2015-01-01 18:45:03 -0700

    sengrid mail recipients

--- app/mailers/playlist_mailer.rb
+++ app/mailers/playlist_mailer.rb
@@ -1,7 +1,6 @@
 class PlaylistMailer < ActionMailer::Base
   include SendGrid
   default from: "thedigitalmusicclub@gmail.com"
-  sendgrid_recipients User.all.map { |user| user.email }
 
   def playlist_email
     mail(:subject => 'The Digital Music Club',
