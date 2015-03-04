cd5fa0bae38ebf1f0f155572785ac42e5d981faf
Seth Musulin <sethmusulin@gmail.com>
2015-03-04 16:34:41 -0700

    adding puma config, procfile and ruby version

--- config/puma.rb
+++ config/puma.rb
@@ -11,5 +11,9 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  on_worker_boot do
+    require 'newrelic_rpm'
+    NewRelic::Agent.manual_start
+  end
   ActiveRecord::Base.establish_connection
 end
